ea318a5f44b527a46c8b843c593089061dd5a442
kavyabhishek <85781922+KAVYABHISHEK@users.noreply.github.com>
2023-07-27 21:16:50 +0530

    Add files via upload

---
+++ Fundamentals_javascript/script.js
@@ -0,0 +1,93 @@
+/*
+const country = 'India' ;
+const continent = 'Asia';
+let population = 1000000 ;
+console.log(country);
+console.log(continent);
+console.log (population);
+let isIsland = false ;
+let language ='telugu';
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+let age = 14; 
+if  (age >= 18) {
+   console.log('Sara can start driving ');
+}
+else {
+    const yearsleft = 18 - age ;
+    console.log(`Sara if too young.Please wait another ${yearsleft} years`);
+}
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+ 
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+ 
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
+
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jhon'));
+console.log(Boolean(null));
+
+const like = prompt("what's your fav num :");
+console.log(like);
+console.log(typeof like);
+
+const favourite = (Number(prompt("what's your fav num :")));
+console.log(favourite);
+
+if (favourite === 23) {
+    console.log('cool ! 23 is a amazing number!');
+} else if (favourite === 7) {
+    console.log('cool ! 7 is a amazing number!');
+} else if (favourite === 9) {
+    console.log('cool ! 9 is a amazing number!');
+} else {
+    console.log('Number is not 23 or 7 or 9');
+}
+if (favourite !== 23) console.log('Why not 23 !');
+
+
+const day = 'Sunday';
+
+switch (day) {
+    case 'Monday':
+        console.log('study well !');
+    break;
+    case 'Tuesday':
+        console.log('write story !');
+        break;
+        case 'Wednesday':
+            console.log('cooking !');
+            break;
+            case 'Thursday':
+                console.log('travel !');
+                break;
+                case 'Friday':
+                    console.log('meeting !');
+                    break;
+                    case 'Saturday':
+                        console.log('study well !');
+                        break;
+                        case 'Sunday':
+                            console.log('Enjoy the weekend !');
+                            break;
+}
+*/
+const age = 16 ;
+const drink = age >= 18 ? 'Drink wine' : 'Drink water';
+age >= 18 ?console.log( 'drink beer') : console.log('drink juice');
+console.log(drink);
